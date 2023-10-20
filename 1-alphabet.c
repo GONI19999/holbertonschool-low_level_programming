5651566be1bf1a41cd97b7803fde65b8de1473fe
GONI19999 <fatmacherif56@gmail.com>
2023-10-20 06:01:05 -0700

    1-alphabet.c

--- 1-alphabet.c
+++ 1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lowercase.
- *
- * Return: Always 0.
- */
-int main(void)
+ * Prints_ alphabet -make the  alphabet
+ * Return: void                                                                         */                                                                                     void print_alphabet(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
 
-	putchar('\n');
+	_putchar('\n');
 }
