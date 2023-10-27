876cdbde57fa15de64e624d987bff1ac48941f4f
GONI19999 <fatmacherif56@gmail.com>
2023-10-27 06:28:44 -0700

    0-memset.c

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,19 +1,30 @@
 #include "main.h"
-/**
- * _memset - function that fills memory with a constant byte
- *@s: first value
- *@b: second value
- *@n: thirth value
- *
- * Return: char with result of memset
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
+#include <stdio.h>
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
-}    
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}   
